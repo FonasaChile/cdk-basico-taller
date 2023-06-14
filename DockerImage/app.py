245bf65924aec51a59c3b4004502a87080ac025c
Elden Jahnke <94935251+ejahnke@users.noreply.github.com>
2023-06-14 11:16:22 -0700

    Update app.py

--- DockerImage/app.py
+++ DockerImage/app.py
@@ -1,4 +1,5 @@
 import boto3
+import os
 import psycopg2
 import json
 from flask import Flask
@@ -6,19 +7,11 @@ app = Flask(__name__)
 
 #s3 = boto3.client('s3')
 secretsClient = boto3.client('secretsmanager',region_name='us-west-2')
-cf_client = boto3.client('cloudformation',region_name='us-west-2')
-cf_stackname = 'FonasaCdkStack'
-cf_response = cf_client.describe_stacks(StackName=cf_stackname)
-cf_outputs = cf_response["Stacks"][0]["Outputs"]
-for output in cf_outputs:
-    keyName = output["OutputKey"]
-    if keyName == "secretName":
-        cf_secret_name = output["OutputValue"]
-
+print("Secret ARN = " + os.environ['SECRETARN'])
 
 @app.route('/')
 def hello():
-	response = secretsClient.get_secret_value(SecretId=cf_secret_name)
+	response = secretsClient.get_secret_value(SecretId=os.environ['SECRETARN'])
 	jsonResponse = json.loads(response['SecretString'])
 	
 	conn = psycopg2.connect(database=jsonResponse['dbname'],host=jsonResponse['host'],user=jsonResponse['username'],password=jsonResponse['password'],port=jsonResponse['port'])
